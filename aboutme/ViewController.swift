601148a9699f10d0f31c25295bb146b57ed7ef12
fontina <fobatt@icloud.com>
2022-06-23 11:51:18 -0500

    yass

--- aboutme/ViewController.swift
+++ aboutme/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var inOutlet: UIButton!
+    
+    @IBOutlet weak var inlabel: UILabel!
+    
+    @IBOutlet weak var buttonIn: UIButton!
+    @IBAction func inButton(_ sender: Any) {
+        inlabel.text = " I like Star Wars, K-Pop, writing, singing, and playing the piano."
+        buttonIn.isHidden = true
+    }
+    
+    @IBOutlet weak var meLabel: UILabel!
+    @IBOutlet weak var buttonMe: UIButton!
+    @IBAction func meButton(_ sender: Any) {
+        meLabel.text = "Born February 17, 2005 and currently attend the Chicago High School for the Arts as a Creative Writer."
+        buttonMe.isHidden = true
+    }
+    
+    @IBOutlet weak var labelDog: UILabel!
+    @IBOutlet weak var buttonDog: UIButton!
+    @IBAction func dogButton(_ sender: Any) {
+        labelDog.text = "This is my dog Cinnamon. She's really mean."
+        buttonDog.isHidden = true
+    }
 }
 
